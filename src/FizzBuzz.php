224273e959d253f88b80d8af2d86cf5bdca2e7e2
Alejandro Irigoyen <irigoyen.161817@e.unavarra.es>
2026-03-12 13:27:22 +0100

    [GREEN] - añadir en codigo return fizz si es 3

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,7 +4,8 @@ namespace Deg540\CleanCodeKata9;
 
 class FizzBuzz
 {
-    public function handle(string $number): string{
+    public function handle(int $number): string{
+        if ($number === 3) return "Fizz";
         return $number;
     }
 }
